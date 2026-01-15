0e9097f0670503221292e04ac1adfdb821f2e872
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-15 10:23:10 +0700

    Replace UDPReceive with UDPRotaryEncoderReceiver in Unity
    Removed the old UDPReceive.cs script and added UDPRotaryEncoderReceiver.cs, which implements UDP listening and parsing for rotary encoder and button events. Updated the corresponding .meta file to match the new script.

---
+++ Test tracker/Assets/RotaryEncoder/UDPRotaryEncoderReceiver.cs
@@ -0,0 +1,55 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UDPRotaryEncoderReceiver : MonoBehaviour
+{
+    private UdpClient udpClient;
+    private Thread thread;
+    private int value;
+    [SerializeField]
+    private int port = 5005;
+
+    void Start()
+    {
+        udpClient = new UdpClient(port);
+        udpClient.EnableBroadcast = true;
+
+        thread = new Thread(Receive);
+        thread.IsBackground = true;
+        thread.Start();
+    }
+    void Receive()
+    {
+        IPEndPoint ep = new IPEndPoint(IPAddress.Any, port);
+        while (true)
+        {
+            byte[] data = udpClient.Receive(ref ep);
+            string message = Encoding.UTF8.GetString(data);
+            if (message.Contains("rotate"))
+                value = int.Parse(message.Split(':')[2].Replace("}", ""));
+            if (message.Contains("button"))
+                Debug.Log("Button clicked");
+        }
+    }
+    void Update()
+    {
+        Debug.Log("Rotary Encoder Value: " + value);
+    }
+    void OnApplicationQuit()
+    {
+        if (thread != null && thread.IsAlive)
+        {
+            thread.Abort();
+        }
+        if (udpClient != null)
+        {
+            udpClient.Close();
+        }
+    }
+}
