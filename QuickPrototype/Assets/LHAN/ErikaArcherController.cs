ad6ece07e3325b1dbad9f15a09fecc935bc84125
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-24 23:54:14 +0700

    Add InputManager and update animation import settings
    Introduced InputManager.cs and its meta file for input handling. Updated animation .fbx.meta files with new compression settings and detailed human/skeleton mapping. Modified ErikaArcherAnim.controller, ErikaArcherController.cs, and WalkForward.unity to integrate new input and animation changes.

--- QuickPrototype/Assets/LHAN/ErikaArcherController.cs
+++ QuickPrototype/Assets/LHAN/ErikaArcherController.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class ErikaArcherController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Animator animator;
+    Vector3 movement;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        movement = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        const float speedRatio = 1f;
+        transform.Translate(movement * Time.deltaTime * speedRatio, Space.World);
+        float forwardSpeed = Vector3.Dot(movement, transform.forward);
+        Debug.Log("forwardSpeed: " + forwardSpeed);
+        animator.SetFloat("forwardSpeed", forwardSpeed);
+        movement = Vector3.zero;
+    }
+    public void MoveForward(float value)
+    {
+        Debug.Log("Move Forward: " + value);
+        movement += transform.forward * value;
+    }
+    public void MoveRight(float value)
+    {
+        Debug.Log("Move Right: " + value);
+        movement += transform.right * value;
+    }
+    public void Rotate(float angle)
+    {
+        transform.Rotate(Vector3.up, angle);
     }
 }
