86c5cb36e48e43473b6c0620fa74693fafa70244
izahai <ngynthhai@gmail.com>
2026-01-24 15:34:59 +0700

    send degree

--- PointEstimator/Broadcaster/udp_broadcast.py
+++ PointEstimator/Broadcaster/udp_broadcast.py
@@ -2,8 +2,10 @@ import json
 import socket
 import time
 
-from Utils.polarUtils import best_fit_3d_circle
-
+from Utils.polarUtils import (
+    best_fit_3d_circle, line_origin_to_highest_y,
+    angle_deg_from_highest
+)
 
 class TrackerUdpBroadcaster:
     def __init__(self, ip="255.255.255.255", port=9000, broadcast=True):
@@ -16,8 +18,15 @@ class TrackerUdpBroadcaster:
             
         # --- Logic ---
         self.circle = None # center(x,y,z) | normal(x,y,z) | radius
+        self.center = None
+        self.v_norm = None
+        self.redius = None
+        self.yline = None
         self.num_point_init = 100
         self._init_points = []
+        
+        self.prev_ang = 0
+        self.prev_time = 0
 
     def close(self):
         self.sock.close()
@@ -32,6 +41,8 @@ class TrackerUdpBroadcaster:
         self._init_points.append([pos[0], pos[1], pos[2]])
         if len(self._init_points) >= self.num_point_init:
             self.circle = best_fit_3d_circle(self._init_points)
+            self.center, self.v_norm, self.radius = self.circle
+            self.yline = line_origin_to_highest_y(self._init_points, self.center)
             self._init_points = []
             return True
         return False
@@ -49,17 +60,41 @@ class TrackerUdpBroadcaster:
             }
         ).encode("utf-8")
         self.sock.sendto(packet, self.addr)
+        
+    def send_degree_position(self, pos):
+        if pos is None:
+            return
+
+        if self.center is None or self.yline is None:
+            return
+
+        origin, highest = self.yline
+        angle_deg = angle_deg_from_highest(origin, highest, pos)
+        ts = time.time()
+        dt = ts - self.prev_time if self.prev_time else 0.0
+        angular_velocity = (angle_deg - self.prev_ang) / dt if dt > 0 else 0.0
+        self.prev_time = ts
+        self.prev_ang = angle_deg
+        
+        packet = json.dumps(
+            {
+                "angle_deg": angle_deg,
+                "angular_velocity": angular_velocity,
+                "ts": ts,
+            }
+        ).encode("utf-8")
+        self.sock.sendto(packet, self.addr)
+        return angle_deg
 
     def send_circle(self):
         if self.circle is None:
             return
-
-        center, normal, radius = self.circle
+        
         packet = json.dumps(
             {
-                "center": center,
-                "normal": normal,
-                "radius": radius,
+                "center": self.center,
+                "normal": self.v_norm,
+                "radius": self.radius,
                 "ts": time.time(),
                 "type": "circle",
             }
