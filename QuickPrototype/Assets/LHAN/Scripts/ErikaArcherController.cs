0d61e4ea916e6c5fa1c2fdb83456c4b60e4673bf
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2026-01-25 22:11:03 +0700

    Add Decimate Grid Master and reorganize assets
    Added the Decimate Grid Master package with models, textures, materials, shaders, and demo scenes. Reorganized LHAN scripts and materials into dedicated folders, removed unused garden and fontana assets, and integrated new plugin and SciFi modular kit demo assets.

---
+++ QuickPrototype/Assets/LHAN/Scripts/ErikaArcherController.cs
@@ -0,0 +1,107 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ErikaArcherController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    Vector3 movement;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        movement = Vector3.zero;
+        SetStairStatusMaterial(StairStatus.Level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementRequest();
+        HandleStair();
+    }
+    public void MoveForward(float value)
+    {
+        // Debug.Log("Move Forward: " + value);
+        movement += transform.forward * value;
+    }
+    public void MoveRight(float value)
+    {
+        // Debug.Log("Move Right: " + value);
+        movement += transform.right * value;
+    }
+    public void Rotate(float angle)
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(Vector3.up * angle));
+    }
+    private void HandleMovementRequest()
+    {
+        const float speedRatio = 1f;
+        rb.MovePosition(rb.position + movement * Time.deltaTime * speedRatio);
+        float forwardSpeed = Vector3.Dot(movement, transform.forward);
+        // Debug.Log("forwardSpeed: " + forwardSpeed);
+        animator.SetFloat("forwardSpeed", forwardSpeed);
+        movement = Vector3.zero;
+    }
+    [SerializeField] private List<Material> stairStatusMaterials;
+    private enum StairStatus
+    {
+        Level,
+        Up,
+        Down
+    }
+    [SerializeField] private float StepHeight = 0.45f;
+    [SerializeField] private float StepSmooth = 0.2f;
+    [SerializeField] private GameObject StepRayUpper;
+    [SerializeField] private GameObject StepRayLower;
+    [SerializeField] private GameObject DebugSphere;
+    private bool IsMovingForward()
+    {
+        float forwardSpeed = animator.GetFloat("forwardSpeed");
+        return forwardSpeed > 0.2f;
+    }
+    private void ClimbStairs()
+    {
+        if (!IsMovingForward()) return;
+        RaycastHit hitLower;
+        if (Physics.Raycast(StepRayLower.transform.position, transform.forward, out hitLower, 0.3f))
+        {
+            Debug.Log("Hit Lower Step: " + hitLower.collider.name);
+            if (!Physics.Raycast(StepRayUpper.transform.position, transform.forward, 0.6f))
+            {
+                Debug.Log("No Hit Upper Step, Climbing Up");
+                Vector3 newPosition = rb.position;
+                newPosition.y += StepSmooth;
+                rb.MovePosition(newPosition);
+                TimeSinceLastClimbUp = 0f;
+                IsClimbingUp = true;
+            }
+        }
+    }
+    private void HandleStair()
+    {
+        TimeSinceLastClimbUp += Time.deltaTime;
+        
+        ClimbStairs();
+        const float climbUpCooldown = 0.7f;
+        if (TimeSinceLastClimbUp > climbUpCooldown)
+        {
+            IsClimbingUp = false;
+            animator.SetBool("isClimbingUp", false);
+            SetStairStatusMaterial(StairStatus.Level);
+        } else
+        {
+            animator.SetBool("isClimbingUp", true);
+            SetStairStatusMaterial(StairStatus.Up);
+        }
+    }
+    private void SetStairStatusMaterial(StairStatus status)
+    {
+        // set the material of the DebugSphere based on the stair status
+        Renderer renderer = DebugSphere.GetComponent<Renderer>();
+        renderer.material = stairStatusMaterials[(int)status];
+    }
+    private bool IsClimbingUp = false;
+    private float TimeSinceLastClimbUp = 0f;
+}
